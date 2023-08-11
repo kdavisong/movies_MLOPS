4a35ef729cabd656042028154caa7ccd28159c4b
kdavisong <101996762+kdavisong@users.noreply.github.com>
2023-08-11 01:32:49 -0500

    test sist recomendacion

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics.pairwise import linear_kernel
+
+df = pd.read_csv("movies_data.csv", index_col = [0])
+crew = pd.read_csv("Crew.csv", index_col = [0])
+genres = pd.read_csv("genres.csv", index_col = [0])
+paises = pd.read_csv("production_countries.csv")
+saga = pd.read_csv("saga.csv")
+mask = (crew["job"] == "Director")
+crew = crew[mask]
 
 app = FastAPI()
 
-movies_dataset = pd.read_csv("movies_data.csv")
-Paises = pd.read_csv("Production_countries.csv")
+
+p = df.duplicated("id_movie")
+df = df.drop(df[p].index)
+df = df.dropna(subset=["title"])
+
+df_recomendado = df.loc[lambda df:(df["vote_average"] > 6)]
+df_recomendado["release_year"] = df_recomendado["release_year"].to_string()
+df_recomendado = df_recomendado[["id_movie","overview","title","vote_average","vote_count","release_year","genres"]]
+df_recomendado["overview"] = df_recomendado["overview"].str.lower()
+
+df_recomendado = pd.merge(df_recomendado, crew[["name","id_movie"]], on = "id_movie")
+df_recomendado["r"] = df_recomendado["genres"] + df_recomendado["overview"] + df_recomendado["name"] 
+tfidf = TfidfVectorizer(stop_words='english')
+tfidf_matrix = tfidf.fit_transform(df_recomendado["r"])
+
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
 
 @app.get("/idiomas/{Idiomas}")
 def obtener_Idiomas(Idiomas:str):
     try:
-        cantidad = str(movies_dataset["original_language"].value_counts()[Idiomas])
+        cantidad = str(df["original_language"].value_counts()[Idiomas])
         return {"Idioma": Idiomas, "Cantidad": cantidad}
     except:
         return("El Idioma ingresado no se encuentra en la base de datos")
@@ -20,7 +46,7 @@ def obtener_Idiomas(Idiomas:str):
 def peliculas_duracion(pelicula:str):
     try:
         pelicula = str.lower(pelicula)
-        resultado = movies_dataset[movies_dataset["title"].str.lower() == pelicula]
+        resultado = df[df["title"].str.lower() == pelicula]
         anio = resultado["release_year"][0]
         duracion = resultado["runtime"][0]
         return {'pelicula':pelicula.upper(), 'duracion':duracion, 'anio':anio}
@@ -30,8 +56,8 @@ def peliculas_duracion(pelicula:str):
 @app.get('/peliculas_pais/{pais}')
 def peliculas_pais(pais:str):
     pais = str.lower(pais)
-    Paises["name"] = Paises["name"].str.lower()
-    cantidad = str(Paises["name"].value_counts()[pais])
+    paises["name"] = paises["name"].str.lower()
+    cantidad = str(paises["name"].value_counts()[pais])
     return {'pais':pais.upper(), 'cantidad':cantidad}
 
 @app.get('/productoras_exitosas/{productora}')
@@ -39,7 +65,7 @@ def productoras_exitosas(productora:str):
     Productoras = pd.read_csv("Productoras.csv")
     Productoras = Productoras["name"].str.replace(" ","_")
     productora = str.replace(" ","_")
-    Productoras = pd.merge(Productoras, movies_dataset, on = "id_movie")
+    Productoras = pd.merge(Productoras, df, on = "id_movie")
     mascara = Productoras["name"] == productora
     revenue = Productoras[mascara]["revenue"].sum()
     cantidad = str(Productoras["name"].value_counts()[productora])
@@ -50,7 +76,7 @@ def get_director(nombre_director:str):
     Crew = pd.read_csv("Crew.csv")
     mask = (Crew["job"] == "Director") & (Crew["name"] == nombre_director)
     Crew = Crew[mask]
-    Crew = pd.merge(Crew, movies_dataset, on = "id_movie")
+    Crew = pd.merge(Crew, df, on = "id_movie")
     retorno_total = Crew["revenue"].sum()
     peliculas = Crew["title"].to_list()
     anio = Crew["release_year"].to_list()
@@ -67,8 +93,20 @@ def franquicia(franquicia:str):
     franquicia = str.lower(franquicia)
     resultado = Saga[Saga["name"].str.lower() == franquicia]
     cantidad = len(resultado)
-    resultado = pd.merge(resultado, movies_dataset, on = "id_movie")
+    resultado = pd.merge(resultado, df, on = "id_movie")
     ganancia_total = resultado["revenue"].sum()
     ganancia_promedio = ganancia_total / cantidad
     return {'franquicia':franquicia, 'cantidad':cantidad, 'ganancia_total':ganancia_total, 'ganancia_promedio':ganancia_promedio}
 
+
+@app.get('/recomendacion/{titulo}')
+
+def busqueda(titulo:str, cosine_sim = cosine_sim):
+    titulo = str.lower(titulo)
+    idx = df_recomendado[df_recomendado["title"].str.lower() == titulo].index.to_list()[0]
+    puntaje_coseno = enumerate(cosine_sim[idx])
+    puntaje_coseno = sorted(puntaje_coseno, key = lambda x: x[1], reverse = True)    
+    puntaje_coseno = puntaje_coseno[1:5]
+    cos_indices = [i[0] for i in puntaje_coseno]
+    resultado = df_recomendado["title"].iloc[cos_indices]
+    return {'lista recomendada': resultado}
